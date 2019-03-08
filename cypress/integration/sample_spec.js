6f93aa762bf343103fa7213495fe49c8e96ddc7d
Nathan Khoo <natebakescakes@gmail.com>
2019-03-08 13:59:31 +0800

    Config CircleCI integration

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,11 +1,41 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+describe('Home Page Renders', () => {
+  it('Visits Employee Profile', () => {
+    cy.visit('localhost:3000');
   });
-});
 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false);
+  it('First Name can be typed', () => {
+    cy.get('[placeholder="First name"]')
+      .type('Alex')
+      .should('have.value', 'Alex');
+  });
+
+  it('Last Name can be typed', () => {
+    cy.get('[placeholder="Last name"]')
+      .type('Test')
+      .should('have.value', 'Test');
+  });
+
+  it('Address can be typed', () => {
+    cy.get('[placeholder="Address"]')
+      .type('123 Test Road')
+      .should('have.value', '123 Test Road');
+  });
+
+  it('Email can be typed', () => {
+    cy.get('[placeholder="Email Address"]')
+      .type('test@test.com')
+      .should('have.value', 'test@test.com');
+  });
+
+  it('Phone can be typed', () => {
+    cy.get('[placeholder="Phone Number"]')
+      .type('123456789')
+      .should('have.value', '123456789');
+  });
+
+  it('Submit button is visible', () => {
+    cy.get('button')
+      .contains('Submit')
+      .should('be.visible');
   });
 });
