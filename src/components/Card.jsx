3f9e670cd3173d4eda76431abbdd8158cc668e52
Nathan Khoo <natebakescakes@gmail.com>
2019-03-11 16:49:19 +0800

    Add modal confirmation for delete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Card, Icon } from 'semantic-ui-react';
-import { StyledButton } from './Styled';
+import React, { useState } from 'react';
+import {
+  Card, Icon, Modal, Button,
+} from 'semantic-ui-react';
+import { StyledButton, StyledCard } from './Styled';
 import { deleteEmployee } from '../API/APICalls';
 
 const EmployeeCard = (props) => {
@@ -8,36 +10,71 @@ const EmployeeCard = (props) => {
   const {
     id, firstName, lastName, address, email, phone,
   } = person;
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
 
   const handleDelete = async () => {
-    await deleteEmployee(id);
+    await deleteEmployee(id).then(() => closeModal());
   };
 
   return (
-    <Card>
-      <Card.Content>
-        <Card.Header>
-          {`${firstName} ${lastName}`}
-          <StyledButton icon onClick={handleDelete}>
-            <Icon name="trash alternate outline" />
-          </StyledButton>
-        </Card.Header>
-        <Card.Description>
-          <p>
-            <Icon name="location arrow" />
-            {address}
-          </p>
-          <p>
-            <Icon name="envelope outline" />
-            {email}
-          </p>
-          <p>
-            <Icon name="phone" />
-            {phone}
-          </p>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+    <StyledCard>
+      <Card>
+        <Card.Content>
+          <Card.Header>
+            {`${firstName} ${lastName}`}
+            <StyledButton icon onClick={showModal}>
+              <Icon name="trash alternate outline" />
+            </StyledButton>
+          </Card.Header>
+          <Card.Description>
+            <p>
+              <Icon name="location arrow" />
+              {address}
+            </p>
+            <p>
+              <Icon name="envelope outline" />
+              {email}
+            </p>
+            <p>
+              <Icon name="phone" />
+              {phone}
+            </p>
+          </Card.Description>
+        </Card.Content>
+      </Card>
+      <Modal size="mini" open={open} onClose={closeModal} dimmer="blurring">
+        <Modal.Header>
+          Delete
+          {' '}
+          {firstName}
+          {' '}
+          {lastName}
+        </Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this card?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button negative onClick={closeModal}>
+            No
+          </Button>
+          <Button
+            positive
+            onClick={handleDelete}
+            icon="checkmark"
+            labelPosition="right"
+            content="Yes"
+          />
+        </Modal.Actions>
+      </Modal>
+    </StyledCard>
   );
 };
 
