20383f6f3a5293744441e11c234656128a418a73
Nathan Khoo <natebakescakes@gmail.com>
2019-03-12 15:04:37 +0800

    Implement delete employee integration testt

---
+++ cypress/integration/delete_employee.js
@@ -0,0 +1,30 @@
+describe('Can delete employee card', () => {
+  before(() => {
+    cy.visit('localhost:3000');
+
+    cy.server();
+    cy.fixture('employee.json').as('employeeJson');
+    cy.route('POST', 'employee', '@employeeJson');
+    cy.get('[placeholder="First name"]').type('Alex');
+    cy.get('[placeholder="Last name"]').type('Test');
+    cy.get('[placeholder="Address"]').type('123 Test Road');
+    cy.get('[placeholder="Email Address"]').type('test@test.com');
+    cy.get('[placeholder="Phone Number"]').type('123456789');
+
+    cy.get('form[id="new-employee"]').submit();
+    cy.contains('You have successfully added an employee');
+    cy.get('.App-header').click();
+  });
+
+  it('Card is deleted', () => {
+    cy.server();
+    cy.route('DELETE', 'api/employee/*', { status: 'success' }).as('deleteEmployee');
+    cy.get('i[class*="trash"')
+      .first()
+      .click();
+    cy.get('button[class*="ui icon positive right labeled button"]').click();
+    cy.wait('@deleteEmployee')
+      .its('responseBody')
+      .should('have.property', 'status', 'success');
+  });
+});
